9108a46f6de403ee5602194e511231bab8f17f78
FFJ1 <69898807+FFJ1@users.noreply.github.com>
2025-07-13 00:24:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let GenKey = () => {
+	setTimeout(() => {
+		window.location.href = "https://ads.luarmor.net/get_key?for=FFJ_Hub-oWTwTDYRLnGv"
+	}, 1000)
+}
+
+let BuyKey = () => {
+	setTimeout(() => {
+		window.location.href = "https://ffj.sellsn.io"
+	}, 1000)
+}
